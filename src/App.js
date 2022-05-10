9249706383e8e55304ed80b2989d3400d6b6e692
amachkel <amachkel@gmail.com>
2022-05-10 11:29:20 -0400

    set up display for weather data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import React from "react";
 
+const api = {
+  key: "d0ec0bc12a2d2e358f70d304e2a267fd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 function App() {
-  return <div className="App"></div>;
+  const dateBuilder = (d) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°c</div>
+          <div className="weather">sunny</div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
